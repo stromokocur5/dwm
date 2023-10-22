849a8b7d8f0e939a8de28cedbf0248ad4724b9ce
stromokocur5 <gabrielkohut3@proton.me>
2023-10-22 19:54:17 +0200

    instead of fish i start tmux in st

--- config.def.h
+++ config.def.h
@@ -4,8 +4,8 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayonleft  = 1;   /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "st", "-e","/usr/bin/fish", NULL };
+static const char *termcmd[]  = { "st", "-e","tmux", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 feh --bg-fill ~/.config/dwm/dwm.jpg &
 picom &
+nm-applet &
 ~/.config/dwm/bar.sh &
 dwm
